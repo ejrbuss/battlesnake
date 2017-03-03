0e5928c57b17b3f5580a74bf4f219f593b1c3380
Eric Buss <ebuss@ualberta.ca>
2017-03-03 15:23:11 -0800

    stuff

--- ai/core.js
+++ ai/core.js
@@ -1,10 +1,16 @@
-function start(req) {
-
-}
+const type = require('../lib/type');
 
 function move(req) {
 
-    return 'up';
+    // Validate request
+    type(req.you).assert.string;
+    type(req.game_id).assert.string;
+    type(req.width).assert.integer;
+    type(req.height).assert.integer;
+    type(req.turn).assert.integer;
+    type(req.food).assert.array;
+    type(req.snakes).assert.object;
+
 }
 
-module.exports = { start, move };
+module.exports = { move };
--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,27 @@ const express = require('express');
 const router  = express.Router();
 
 // Handle POST request to '/start'
-router.post('/start', (req, res) => {
-    ai.start(req);
-    // Response data
-    return res.json({
+router.post('/start', (req, res) =>
+    res.json({
         color    : "#DFFF00",
         name     : "Snek",
         head_url : "http://www.placecage.com/c/200/200", // optional, but encouraged!
         taunt    : "Let's do thisss thang!",             // optional, but encouraged!
-    });
-})
+    })
+);
 
 // Handle POST request to '/move'
 router.post('/move', (req, res) => {
+    let move;
+    try {
+        move = ai.move(req.body);
+    } catch(err) {
+        console.log(err);
+    }
     res.json({
-        move  :  ai.move(req),          // one of: ['up','down','left','right']
+        move  :  move || 'up',          // one of: ['up','down','left','right']
         taunt : 'Outta my way, snake!', // optional, but encouraged!
-    });
-})
+    })
+});
 
 module.exports = router;
